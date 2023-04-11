1e3865936ed7aa1368baf00e69858893a940d797
tjrcks630 <tjrcks630@gmail.com>
2023-04-11 15:25:19 +0900

    CRUD_Templet

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require("sync-mysql")
-const env = require("dotenv").config({ path: "../../.env" });
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('sync-mysql');
+const env = require('dotenv').config({ path: "../../.env" });
 
 var connection = new mysql({
     host: process.env.host,
@@ -17,8 +17,66 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/hello", (req, res) => {
-    res.send("Hello World")
+app.get('/hello', (req, res) => {
+    res.send('Hello World~!!')
+})
+
+app.get('/select', (req, res) => {
+    const result = connection.query('select * from user');
+    console.log(result);
+    res.send(result);
+})
+
+app.post('/select', (req, res) => {
+    const result = connection.query('select * from user');
+    console.log(result);
+    res.send(result);
+})
+
+// request O, query O
+app.get('/selectQuery', (req, res) => {
+    const userid = req.query.userid;
+    const result = connection.query("select * from user where userid=?", [userid]);
+    console.log(result);
+    res.send(result);
+})
+
+// request O, query O
+app.post('/selectQuery', (req, res) => {
+    const userid = req.body.userid;
+    const result = connection.query("select * from user where userid=?", [userid]);
+    console.log(result);
+    res.send(result);
+})
+
+// request O, query O
+app.post('/insert', (req, res) => {
+    const { id, pw } = req.body;
+    const result = connection.query("insert into user values (?, ?)", [id, pw]);
+    console.log(result);
+    res.send(result);
+})
+
+
+app.post('/insert', (req, res) => {
+    const { id, pw } = req.body;
+    const result = connection.query("insert into user values (?, ?)", [id, pw]);
+    console.log(result);
+    res.resredirect('/selectQuery?userid=' + req.body.id);
+})
+
+app.post('/update', (req, res) => {
+    const { id, pw } = req.body;
+    const result = connection.query("update user set passwd=? where userid=?", [pw, id]);
+    console.log(result);
+    res.redirect('/selectQuery?userid=' + req.body.id);
+})
+
+app.post('/delete', (req, res) => {
+    const id = req.body.id;
+    const result = connection.query("delete from user where userid=?", [id]);
+    console.log(result);
+    res.redirect('/select');
 })
 
 module.exports = app;
