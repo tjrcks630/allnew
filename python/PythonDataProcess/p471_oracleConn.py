7cc8a6c056e122ec4a2f195e9823c948804951c4
tjrcks630 <tjrcks630@gmail.com>
2023-05-16 11:47:22 +0900

    Python_Uploads

--- python/PythonDataProcess/p471_oracleConn.py
+++ python/PythonDataProcess/p471_oracleConn.py
@@ -1,11 +1,13 @@
 import cx_Oracle
 
-cx_Oracle_orcle_client(lib_dir="C:\OracleXE\instantclient_19_18")
-conn = None #접속 객체
-cur = None #커서 객체
+# cx_Oracle.init_oracle_client(lib_dir="/Users/Lune/OracleXE/instantclient_19_8")
+cx_Oracle.init_oracle_client(lib_dir="/usr/local/OracleXE/instantclient_19_19")
+
+conn = None  # 접속 객체
+cur = None  # 커서 객체
 
 try:
-    # 아이디/비번@hastname:port_number/sid
+    # 아이디/비번@hostname:port_number/sid
     loginfo = 'hr/1234@192.168.1.149:1521/xe'
     conn = cx_Oracle.connect(loginfo)
     print(type(conn))
@@ -13,8 +15,8 @@ try:
     cur = conn.cursor()
     print(type(cur))
 
-    sql = 'select power (2,10) from dual'
-    cur.excute(sql)
+    sql = 'select power(2, 10) from dual'
+    cur.execute(sql)
 
     for item in cur:
         print(item)
